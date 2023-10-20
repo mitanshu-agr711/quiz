150044ef07f6735266c27ab43a678f7cc555f442
Mitanshu Agrawal <144200829+mitanshu-agr711@users.noreply.github.com>
2023-10-21 05:15:29 +0530

    local storage stores the data

--- info.js
+++ info.js
@@ -1,1 +1,20 @@
-document.getElementsByClassName();
+let form = document.querySelector("form");
+console.log(form);
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+    let name = event.target.username.value;
+    let email = event.target.email.value;
+    let section = event.target.section.value;
+    let roll = event.target.roll.value;
+
+    var userDetails = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+    let userData = {
+        'name': name,
+        'email': email,
+        'section': section,
+        'roll no.': roll 
+    };
+    userDetails.push(userData);
+    localStorage.setItem("userDetails", JSON.stringify(userDetails));
+    console.log(userDetails);
+});
