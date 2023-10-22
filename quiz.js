a0e50662f49983fc16611fc3bd0ee42c0efabaf0
Mitanshu Agrawal <144200829+mitanshu-agr711@users.noreply.github.com>
2023-10-22 21:13:54 +0530

    now play button added

--- quiz.js
+++ quiz.js
@@ -35,14 +35,14 @@ let quiz =
             Options: ["J.K. Rowling", " George Orwell", "Mark Twain", "Harper Lee"],
             Answer: 4,
         }];
-        let times=10;
+let times = 10;
 let currentindex = 0;
 let Attemps = 0;
 let correct = 0;
 let wrong = 0;
 let timeupshogaya = true;
 printQuestion(currentindex);
- 
+
 function printQuestion(i) {
 
     // console.log(quiz[0]);
@@ -52,40 +52,39 @@ function printQuestion(i) {
     document.querySelectorAll(".option li")[2].textContent = quiz[i].Options[2];
     document.querySelectorAll(".option li")[3].textContent = quiz[i].Options[3];
     let timerElement = document.querySelector("#time");
-    let remainingTime=times;
-    function uptime(){
+    let remainingTime = times;
+    function uptime() {
         min = Math.floor(remainingTime / 60);
         sec = remainingTime % 60;
-    timerElement.textContent = min + ":" + sec;
-    if(remainingTime==0)
-    {
-        timeupshogaya=false;
-       alert("Time's up for this question!");
+        timerElement.textContent = min + ":" + sec;
+        if (remainingTime == 0) {
+            timeupshogaya = false;
+            alert("Time's up for this question!");
+        }
+        else {
+            remainingTime--;
+            setTimeout(uptime, 1000)
+        }
     }
-    else{
-        remainingTime--;
-        setTimeout(uptime, 1000)
-    }
-}
-uptime();
+    uptime();
 }
 
 let optionClicked = 0
-if(timeupshogaya){
-function checkAnswer(Options) {
+if (timeupshogaya) {
+    function checkAnswer(Options) {
 
-    // let optionClicked=document.querySelector(".option li").isDefaultNamespace("opt");
-    // console.log(optionClicked);
-    optionClicked = Options.getAttribute("data-opt");
-    // console.log(optionClicked);
-    console.log(optionClicked);
-    if (optionClicked == quiz[currentindex].Answer) {
-        correct++;
+        // let optionClicked=document.querySelector(".option li").isDefaultNamespace("opt");
+        // console.log(optionClicked);
+        optionClicked = Options.getAttribute("data-opt");
+        // console.log(optionClicked);
+        console.log(optionClicked);
+        if (optionClicked == quiz[currentindex].Answer) {
+            correct++;
+
+        }
+        else { wrong++; }
 
     }
-    else { wrong++; }
-    
-}
 }
 let result;
 function nextQuestion() {
@@ -98,7 +97,9 @@ function nextQuestion() {
         } else {
             alert("Quiz completed!");
             result = confirm('If you want to see the scroe, please enter ok otherwise enter cancle');
-
+            if (result) {
+                showResult();
+            }
         }
     }
     optionClicked = 0;
@@ -111,6 +112,9 @@ function SkipQuestion() {
     } else {
         alert("Quiz completed!");
         result = confirm('If you want to see the scroe, please enter ok otherwise enter cancle');
+        if (result) {
+            showResult();
+        }
     }
 }
 let score = correct * 5 - wrong * 1;
@@ -134,7 +138,14 @@ function Showresult() {
 
 }
 
-
+function hidden() {
+    document.getElementById("quizz").style.display = "none";
+}
+setTimeout("hidden()", 60000);
+function show() {
+    document.getElementById("result").style.display = "block"
+}
+setTimeout("show()", 60001);
 // localStorage.setItem('quizresult',JSON.stringify(Showresult));
 // document.querySelector('.quiz').style.display = 'none';
 // document.querySelector('.result-show').removeAttribute('hidden');
@@ -150,3 +161,11 @@ function Showresult() {
 //         Showresult();
 //     }
 // });
+
+// console.log(quiz[0]);
+function showResult() {
+    document.querySelector("#attem").textContent = Attemps;
+    document.querySelector("#cort").textContent = correct;
+    document.querySelector("#wrog").textContent = wrong;
+    document.querySelector("#scor").textContent = score;
+}
